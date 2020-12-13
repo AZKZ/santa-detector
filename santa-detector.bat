2e5ad3bd42e7c63404db1105411617d4b324c55c
kazuya azegami <aze.gk.hh.esp@gmail.com>
2020-12-14 06:34:05 +0900

    実行用のスクリプトを新規作成した。

---
+++ santa-detector.bat
@@ -0,0 +1,7 @@
+set IMAGE_SAVE_DIR=/xxxx/xxxx/xxxx/xxx
+set GOOGLE_APPLICATION_CREDENTIALS=/xxxx/xxxx/xxxx/xxx/xxxx.json
+set SLACK_BOT_TOKEN=xoxb-xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
+set SLACK_CHANNEL=#general
+set VIDEO_CAPTURE_INDEX=0
+
+gradlew.bat run
