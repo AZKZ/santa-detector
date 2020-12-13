d9e2856dc649988b12b42f1ebd3adca9b106372a
kazuya azegami <aze.gk.hh.esp@gmail.com>
2020-12-13 09:25:51 +0900

    SlackMessenger.javaの無駄なソースの削除して、コメントを追加した。

--- src/main/java/azkz/notification/SlackMessenger.java
+++ src/main/java/azkz/notification/SlackMessenger.java
@@ -8,48 +8,37 @@ import java.io.File;
 import java.io.IOException;
 import java.util.List;
 
+/**
+ * Slackにメッセージを送るクラス
+ */
 public class SlackMessenger {
 
     private String token;
 
+    /**
+     * コンストラクタ
+     * @param token botのトークン([xoxb-]から始まるもの)
+     */
     public SlackMessenger(String token) {
         this.token = token;
     }
 
-    public void postMessage(String channel, String text) {
+    /**
+     * @param channels チャンネルID or チャンネル名
+     * @param comment  アップロード時のコメント
+     * @param file     アップロードするファイル
+     */
+    public void uploadFile(List<String> channels, String comment, File file) throws IOException, SlackApiException {
         var client = Slack.getInstance().methods();
+        // TODO ログを出力できるようにする。できればエラー原因もわかるように。
         var logger = LoggerFactory.getLogger("my-awesome-slack-app");
-        try {
-            // Call the chat.postMessage method using the built-in WebClient
-            var result = client.chatPostMessage(r -> r
-                            // The token you used to initialize your app
-                            .token(token)
-                            .channel(channel)
-                            .text(text)
-                    // You could also use a blocks[] array to send richer content
-            );
-            // Print result, which includes information about the message (like TS)
-            logger.info("result {}", result);
-        } catch (IOException | SlackApiException e) {
-            logger.error("error: {}", e.getMessage(), e);
-        }
+        var result = client.filesUpload(r -> r
+                .token(token)
+                .channels(channels)
+                .file(file)
+                .filename(file.getName())
+                .initialComment(comment)
+        );
     }
-
-    public void uploadFile(List<String> channels, String comment, File file){
-        var client = Slack.getInstance().methods();
-        try {
-            var result = client.filesUpload(r -> r
-                    .token(token)
-                    .channels(channels)
-                    .file(file)
-                    .filename(file.getName())
-                    .initialComment(comment)
-                    );
-            System.out.println(result);
-        }catch (Exception e){
-            e.printStackTrace();
-        }
-    }
-
 
 }
