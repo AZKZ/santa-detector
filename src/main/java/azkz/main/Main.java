b16191ce8cd9c58c146cba8d1deb3bb79bf8a921
kazuya azegami <aze.gk.hh.esp@gmail.com>
2020-12-13 13:28:05 +0900

    Main.java の処理を変更した。まだ仮。

--- src/main/java/azkz/main/Main.java
+++ src/main/java/azkz/main/Main.java
@@ -1,79 +1,42 @@
 package azkz.main;
 
 import azkz.camera.Camera;
-import org.apache.http.HttpEntity;
-import org.apache.http.client.methods.CloseableHttpResponse;
-import org.apache.http.client.methods.HttpPost;
-import org.apache.http.client.utils.URIBuilder;
-import org.apache.http.entity.FileEntity;
-import org.apache.http.impl.client.CloseableHttpClient;
-import org.apache.http.impl.client.HttpClientBuilder;
-import org.apache.http.util.EntityUtils;
-import org.json.JSONObject;
+import azkz.imagerecognition.GoogleVisionApi;
+import azkz.messenger.SlackMessenger;
+import com.google.cloud.vision.v1.EntityAnnotation;
 
 import java.io.File;
-import java.net.URI;
-import java.util.ResourceBundle;
+import java.util.Arrays;
+import java.util.List;
 
 public class Main {
 
     public static void main(String[] args) {
-        // プロパティファイル(src/main/resources/env.properties)を読み込む。
-        ResourceBundle resourceBundle = ResourceBundle.getBundle("env");
-
         try {
+            // 写真を撮影し、保存された画像ファイルの絶対パスを取得する。
+            String imageFilePath = Camera.takePicture(System.getenv("IMAGE_DIST_DIR"));
 
-            // 写真を撮影し、保存された画像の絶対パスを取得する。
-            String imageFilePath = Camera.takePicture(resourceBundle.getString("imageFileDir"));
+            // 絶対パスからFileオブジェクトを作成する。
+            File imageFile = new File(imageFilePath);
 
+            // Google Vision APIで画像のラベル検出を行う。
+            List<EntityAnnotation> labels = GoogleVisionApi.detectLabels(imageFile);
 
-            // HTTPクライアントを作成する。
-            CloseableHttpClient httpClient = HttpClientBuilder.create().build();
+            // [santa claus]が含まれているかチェックする。
+            boolean containsSanta = labels.stream().anyMatch(entityAnnotation ->
+                    entityAnnotation.getDescription().toLowerCase().contains("santa claus"));
 
-
-            URIBuilder builder = new URIBuilder(resourceBundle.getString("endpoint") + "vision/v3.1/analyze");
-
-            // オプションのリクエストパラメーターを設定する。
-            //   visualFeaturesは画像の特徴を返すオプション
-            //   他にもBrandsやFaces、Tagsなどの値をカンマ区切りで設定できる。(https://westcentralus.dev.cognitive.microsoft.com/docs/services/computer-vision-v3-1-ga/operations/56f91f2e778daf14a499f21b)
-            builder.setParameter("visualFeatures", "Description,Tags");
-
-            // POSTリクエストを準備する。
-            URI uri = builder.build();
-            HttpPost request = new HttpPost(uri);
-            request.setHeader("Content-Type", "application/octet-stream");
-            request.setHeader("Ocp-Apim-Subscription-Key", resourceBundle.getString("subscriptionKey"));
-            FileEntity fileEntity = new FileEntity(new File(imageFilePath));
-
-            request.setEntity(fileEntity);
-
-            // リクエストを送信し、レスポンスを受信する。
-            CloseableHttpResponse response = httpClient.execute(request);
-            HttpEntity entity = response.getEntity();
-
-            if (entity != null) {
-                // レスポンスのJSONを整形して、出力する。
-                String jsonString = EntityUtils.toString(entity);
-                JSONObject json = new JSONObject(jsonString);
-                System.out.println("REST Response:\n");
-                System.out.println(json.toString(2));
-                System.out.println();
-
-                // JSONの文字列の中に「santa」が含まれているか判定する。
-                if (jsonString.toLowerCase().contains("santa")) {
-                    System.out.println("☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆");
-                    System.out.println("サンタさんが来た！！！！");
-                    System.out.println("☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆");
-                } else {
-                    System.out.println();
-                    System.out.println("サンタさん来ないなぁ。。。");
-                    System.out.println();
-                }
+            // 含まれていたらSlackに画像ファイルをアップロードする。
+            if (containsSanta) {
+                System.out.println("サンタいた");
+                SlackMessenger messenger = new SlackMessenger(System.getenv("SLACK_BOT_TOKEN"));
+                messenger.uploadFile(Arrays.asList(System.getenv("SLACK_CHANNEL")), "サンタが出たぞ！！！", imageFile);
+            } else {
+                System.out.println("サンタいなかった。。。");
             }
+
         } catch (Exception e) {
             e.printStackTrace();
         }
-
-
     }
 }
