1e926013b90d4c811e2f327a46225bbe65104bf3
kazuya azegami <aze.gk.hh.esp@gmail.com>
2020-12-18 19:29:16 +0900

    Windows環境での試しを受けて、修正を加えた。

--- santa-detector.bat
+++ santa-detector.bat
@@ -1,7 +1,10 @@
-set IMAGE_SAVE_DIR="/xxxx/xxxx/xxxx/xxx"
-set GOOGLE_APPLICATION_CREDENTIALS="/xxxx/xxxx/xxxx/xxx/xxxx.json"
-set SLACK_BOT_TOKEN="xoxb-xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx"
-set SLACK_CHANNEL="#general"
-set VIDEO_CAPTURE_INDEX="0"
+@echo off
+cd %~dp0
+chcp 65001
+set IMAGE_SAVE_DIR=/xxxx/xxxx/xxxx/xxx
+set GOOGLE_APPLICATION_CREDENTIALS=/xxxx/xxxx/xxxx/xxx/xxxx.json
+set SLACK_BOT_TOKEN=xoxb-xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
+set SLACK_CHANNEL=#general
+set VIDEO_CAPTURE_INDEX=0
 
-gradlew.bat run
+gradlew run
